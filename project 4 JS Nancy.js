7e60373beb48289be4d0959ed2b1956c204643cb
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-12 12:40:11 +0530

    Create project 4 JS Nancy.js

---
+++ project 4 JS Nancy.js
@@ -0,0 +1,23 @@
+let addToDoButton = document.getElementById('addbtn');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('task_input');
+
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.innerText =task_input.value;
+    if(task_input.value == ""){
+        alert("Write something to add!!!");
+    }
+    else{
+        toDoContainer.appendChild(paragraph);
+    }
+    task_input.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+
+}) 
