59efbca4a760a274c5deb178819af43a74adfef9
namratabose32 <74545248+namratabose32@users.noreply.github.com>
2021-01-22 20:54:27 +0530

    Add files via upload
    Portfolio using HTML CSS & JavaScript

---
+++ exp.js
@@ -0,0 +1,6 @@
+var clearToDoButton=document.getElementById("clearBtn");
+var paragraph=document.getElementById("frm1");
+
+clearToDoButton.addEventListener('click',function(){
+    paragraph.innerText="";
+})
